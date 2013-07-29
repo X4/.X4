a81b7e72ec90fdaf950536269394ad04c47b14df
Fernandos <worldwide@online.de>
2013-07-29 17:06:28 +0200

    removing cruft

--- binaries/disable-ati.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mount -t debugfs debugfs /sys/kernel/debug -o remount
-modprobe radeon
-echo "Turning off ATI graphics"
-echo IGD > /sys/kernel/debug/vgaswitcheroo/switch
-echo OFF > /sys/kernel/debug/vgaswitcheroo/switch
-
