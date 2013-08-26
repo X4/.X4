f553364e5d4ff8229898fc4ac17ff81a2f675f8f
Fernandos <worldwide@online.de>
2013-08-27 00:41:10 +0200

    fancier completion styles

--- modules/completion/init.zsh
+++ modules/completion/init.zsh
@@ -42,6 +42,7 @@ source ${0:h}/generic.zsh(.) /dev/null
 #
 
 setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
+setopt HASH_LIST_ALL       # Hash command path is first before running completion.
 setopt ALWAYS_TO_END       # Move cursor to the end of a completed word.
 setopt PATH_DIRS           # Perform path search even on command names with slashes.
 setopt AUTO_MENU           # Show completion menu on a succesive tab press.
@@ -53,6 +54,8 @@ unsetopt FLOW_CONTROL      # Disable start/stop characters in shell editor.
 # Treat these characters as part of a word.
 WORDCHARS='_%'
 
+# Use eight bit characters for completions
+PRINT_EIGHT_BIT=1
 
 #
 # Caching
@@ -73,9 +76,15 @@ if zstyle -t ':zcontrol:module:completion:*' case-sensitive; then
   zstyle ':completion:*'                matcher-list        'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
   setopt CASE_GLOB
 else
-  zstyle ':completion:*'                matcher-list        'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+  if [[ "$LANG" =~ "de" ]]; then
+    zstyle ':completion:*'              matcher-list        'm:ss=ß m:ue=ü m:ue=Ü m:oe=ö m:oe=Ö m:ae=ä m:ae=Ä m:{a-zA-Zöäüa-zÖÄÜ}={A-Za-zÖÄÜA-Zöäü}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+  else
+    zstyle ':completion:*'              matcher-list        'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+  fi
   unsetopt CASE_GLOB
 fi
+# Adjust case-insensitive completions for: (all),partial-word and then substring matches
+
 
 # Group matches and describe.
 zstyle ':completion:*:*:*:*:*'          menu                select
@@ -83,24 +92,45 @@ zstyle ':completion:*:matches'          group               'yes'
 zstyle ':completion:*:options'          description         'yes'
 zstyle ':completion:*:options'          auto-description    '%d'
 zstyle ':completion:*:corrections'      format              ' %F{green}-- %d (errors: %e) --%f'
-zstyle ':completion:*:descriptions'     format              ' %F{yellow}-- %d --%f'
+#zstyle ':completion:*:descriptions'     format              ' %F{yellow}-- %d --%f'
+zstyle ':completion:*:descriptions'     format              $'%{\033[1m⚔\e[0;33m%} completing ☛\e[0m %B%d%b%{\e[0m%}'
 zstyle ':completion:*:messages'         format              ' %F{purple} -- %d --%f'
 zstyle ':completion:*:warnings'         format              ' %F{red}-- no matches found --%f'
 zstyle ':completion:*:default'          list-prompt         '%S%M matches%s'
+zstyle ':completion:*'                  list-colors         'reply=( "=(#b)(*$PREFIX)(?)*=00=$color[green]=$color[bg-green]" )'
+zstyle -e ':completion:*:-command-:*:commands'  list-colors 'reply=( '\''=(#b)('\''$words[CURRENT]'\''|)*-- #(*)=0=38;5;45=38;5;136'\'' '\''=(#b)('\''$words[CURRENT]'\''|)*=0=38;5;45'\'' )'
+zstyle ':completion:*'                  select-prompt %SScrolling active: current selection at %P Lines: %m
 zstyle ':completion:*'                  format              ' %F{yellow}-- %d --%f'
 zstyle ':completion:*'                  group-name          ''
+
+# Enable verbose completions (similar to fish shell)
 zstyle ':completion:*'                  verbose             yes
+zstyle ':completion:*'                  extra-verbose       true
+
+# Enable command descriptions. Disable if too slow
+#zstyle ':completion:*:-command-:*:'    verbose             true
+
+# Prevent menu completion for ambiguous initial strings
+zstyle ':completion:*'                  insert-unambiguous  true
+zstyle ':completion:*:correct:*'        original            true
 
 # Fuzzy match mistyped completions.
 zstyle ':completion:*'                  completer         _complete _match _approximate
 zstyle ':completion:*:match:*'          original          only
 zstyle ':completion:*:approximate:*'    max-errors        1 numeric
 
+# Prevent comp to glob the first part of the path to avoid partial globs. (Performance)
+zstyle ':completion:*'                  accept-exact      '*(N)'
+
+# Enable rehash on completion so new installed program are found automatically:
+_force_rehash() {
+  (( CURRENT == 1 )) && rehash
+  return 1      # Because we didn't really complete anything
+}
+zstyle ':completion:::::'               completer         _force_rehash _complete _approximate
+
 # Increase the number of errors based on the length of the typed word.
 zstyle -e ':completion:*:approximate:*' max-errors      'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
-
-# Don't complete unavailable commands.
-zstyle ':completion:*:functions'        ignored-patterns  '(_*|pre(cmd|exec))'
 
 # Array completion element sorting.
 zstyle ':completion:*:*:-subscript-:*'  tag-order       indexes parameters
@@ -112,6 +142,12 @@ zstyle ':completion:*:*:cd:*:directory-stack' menu      yes select
 zstyle ':completion:*:-tilde-:*'        group-order     'named-directories' 'path-directories' 'users' 'expand'
 zstyle ':completion:*'                  squeeze-slashes true
 
+# Enable . and .. as a completion
+zstyle -e ':completion:*'               special-dirs '[[ $PREFIX = (../)#(|.|..) ]] && reply=(..)'
+
+# Enable .. as a completion
+#zstyle ':completion:*' special-dirs ..
+
 # History
 zstyle ':completion:*:history-words'    stop            yes
 zstyle ':completion:*:history-words'    remove-all-dups yes
@@ -120,6 +156,9 @@ zstyle ':completion:*:history-words'    menu            yes
 
 # Environmental Variables
 zstyle ':completion::*:(-command-|export):*'  fake-parameters  ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
+
+# Enable process lists completion, like the local web server details and host completion
+zstyle ':completion:*:urls'             local 'www' '/var/www/' 'public_html'
 
 # Populate hostname completion.
 zstyle -e ':completion:*:hosts'   hosts  'reply=(
@@ -141,6 +180,23 @@ zstyle ':completion:*:*:*:users'  ignored-patterns \
 # ... unless we really want to.
 zstyle '*'  single-ignored  show
 
+# Prevent CVS files/directories from being completed
+zstyle ':completion:*:(all-|)files'     ignored-patterns '(|*/)CVS'
+zstyle ':completion:*:cd:*'             ignored-patterns '(*/)#CVS'
+
+# Prevent completion of backup files as executables
+zstyle ':completion:*:complete:-command-::commands' ignored-patterns       '*\~'
+
+# Prevent these filename suffixes to ignore during completion (except after rm command)
+zstyle ':completion:*:*:(^rm):*:*files'   ignored-patterns  '*?.(o|c~|old|pro|zwc)' '*~'
+
+# Don't complete unavailable commands.
+zstyle ':completion:*:functions'        ignored-patterns  '(_*|pre(cmd|exec))'
+
+# Prevent files to be ignored from zcompile
+zstyle ':completion:*:*:zcompile:*'     ignored-patterns  '(*~|*.zwc)'
+
+
 # Ignore multiple entries.
 zstyle ':completion:*:(rm|kill|diff):*'   ignore-line       other
 zstyle ':completion:*:rm:*'               file-patterns     '*:all-files'
@@ -161,6 +217,16 @@ zstyle ':completion:*:*:mpg123:*'         file-patterns     '*.(mp3|MP3):mp3\ fi
 zstyle ':completion:*:*:mpg321:*'         file-patterns     '*.(mp3|MP3):mp3\ files *(-/):directories'
 zstyle ':completion:*:*:ogg123:*'         file-patterns     '*.(ogg|OGG|flac):ogg\ files *(-/):directories'
 zstyle ':completion:*:*:mocp:*'           file-patterns     '*.(wav|WAV|mp3|MP3|ogg|OGG|flac):ogg\ files *(-/):directories'
+
+# File sort order defaults to name (incl. when unset or invalid)
+# `size' to sort them by the size of the file
+# `links' to sort them by the number of links to the file
+# `modification' or `time' or `date' to sort them by the last modification time
+# `access' to sort them by the last access time
+# `inode' or `change' to sort them by the last inode change time
+# `reverse' to sort in decreasing order
+# `name` to sort alphabetically by name
+zstyle ':completion:*' file-sort name
 
 # Mutt
 if [[ -s "${XDG_CONFIG_HOME}/mutt/aliases" ]]; then
