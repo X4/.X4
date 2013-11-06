e349b71ff0e66cfb4e584e83e516c8668e370b10
Fernandos <worldwide@online.de>
2013-11-06 17:30:59 +0100

    removing pulseaudios crap and adding systemctl and journalctl comfort

--- local/aliases/services.zsh
+++ local/aliases/services.zsh
@@ -1,5 +1,26 @@
-# PulseAudio
-alias s.sound.stop="pulseaudio -k --cleanup-shm > /dev/null"
-alias s.sound.start="pulseaudio -D --realtime=1 > /dev/null"
-alias s.sound.restart="pulseaudio -k > /dev/null ; killall kmix > /dev/null ; pulseaudio -D --realtime=1 > /dev/null ; kmix --keepvisibility & > /dev/null"
+# control systemd services
+alias s.start="systemctl start"
+alias s.stop="systemctl stop"
+alias s.restart="systemctl restart"
+alias s.status="systemctl status"
 
+# control systemd unit files
+alias s.available="systemctl list-unit-files"
+alias s.enable="systemctl enable"
+alias s.disable"systemctl disable"
+alias s.services="systemctl list-units -t service"
+alias s.dependencies="systemctl list-dependencies"
+alias s.reload="systemctl daemon-reload"
+alias s.reset="systemctl reset-failed
+
+# control pc status using systemd
+alias pc.poweroff="systemctl poweroff"
+alias pc.poweroff="systemctl reboot"
+alias pc.poweroff="systemctl suspend"
+alias pc.poweroff="systemctl hibernate"
+alias pc.poweroff="systemctl hybrid-sleep"
+
+# control journalctl
+alias j.list="journalctl -xn --full"
+alias j.catalog="journalctl --list-catalog"
+alias j.force-rotation="systemctl kill --kill-who=main --signal=SIGUSR2 systemd-journald.service"
