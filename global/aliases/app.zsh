3d89dab57fd498d2d3b00eeaa75e03c6b66d772d
Fernandos <worldwide@online.de>
2014-02-21 13:10:15 +0100

    nicer history

--- global/aliases/app.zsh
+++ global/aliases/app.zsh
@@ -12,3 +12,14 @@ alias ascii='man ascii'
 alias scp='rsync --rsh=ssh -CarvP'
 alias sloc='cloc --by-file-by-lang --exclude-dir .git'
 alias diff='colordiff -u'
+
+# Show history with timestamps
+if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]; then
+    alias history='fc -fl 1'
+elif [ "$HIST_STAMPS" = "dd.mm.yyyy" ]; then
+    alias history='fc -El 1'
+elif [ "$HIST_STAMPS" = "yyyy-mm-dd" ];then
+    alias history='fc -il 1'
+else
+    alias history='fc -l 1'
+fi
